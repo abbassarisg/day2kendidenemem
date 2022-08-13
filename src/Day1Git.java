054c534c6f9334090cbc0e1cb8c9f3eb0cd6e281
Abbas Sarı <abbassari.sg@gmail.com>
2022-08-13 22:00:46 +0300

    3.C

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -1,6 +1,8 @@
 public class Day1Git {
     public static void main(String[] args) {
         System.out.println("Git 1. Deneme");
-        System.out.println("Git 1. Deneme");
+        System.out.println("Git 2. Deneme");
+        System.out.println("Git 3. Deneme");
+
     }
 }
